021a2bd293624904d56249575e25f84f07597603
sosos-618 <fdaldut1212@gmail.com>
2021-07-04 22:01:25 +0900

    playerクラスの追加

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    // Monster monster = new Monster();
+    // System.out.println(monster.toString());
+    Player p = new Player();
+    p.drawMonsters();
+    p.showDeck();
   }
 }
